e735f0f14bb6926f76dcb2e4584435e8af85b149
ValerieEkoume <72264813+ValerieEkoume@users.noreply.github.com>
2020-10-19 15:38:17 +0200

    Ajout elmts Bootstrap

--- Formulaire.php
+++ Formulaire.php
@@ -4,29 +4,40 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
+
     <meta charset="UFT-8">
+
+    <link rel="stylesheet" href="style.css">
     <title>Form PHP</title>
+
 </head>
 
-<body>
-<p>
-<h2>Connectez-vous</h2><br>
-</p>
+<div class="container mt-5">
+    <div class="card border-0 rounded-lg bgpink">
+        <div class="card-body">
+            <h1 class="display-1">Connectez-vous</h1><br>
 
-<form action="Test.php" method="post">
-    <div class="form">
-        <br>
-        <label  for="">email:</label>
-        <input class="email" type="email" name="email" class="form-control"><br>
-        <br>
-        <label for="">password:</label>
-        <input class="password" type="password" name="password" class="form-control"><br>
-        <br>
+                <form action="Test.php" method="post">
+                    <div class="form-group">
+                        <label for="">Email address</label>
+                        <input type="email" class="form-control" name="email" aria-describedby="emailHelp">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="custom-control custom-switch">
+                        <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                        <label class="custom-control-label" for="customSwitch1">Toggle this element</label>
+                    </div>
+                    <div class="text-center mt-2">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+        </div>
     </div>
-
-
-    <input class="send" type="submit" value="Envoyer"><br>
-</form>
+</div>
 </body>
+
 </html>
